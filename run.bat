e4cd13fc51e37d4a3c2e4dc004598e047324a623
Hytoneha <44321649+Hytoneha@users.noreply.github.com>
2018-11-22 09:32:11 +0200

    Code for test cases
    Logs are not uploaded yet. They will be uploaded when they are ran

---
+++ run.bat
@@ -0,0 +1,4 @@
+python test_cases.py .\firmware\tamk_1.bin > log1.txt
+python test_cases.py .\firmware\tamk_2.bin > log2.txt
+python test_cases.py .\firmware\tamk_3.bin > log3.txt
+python test_cases.py .\firmware\tamk_4.bin > log4.txt
